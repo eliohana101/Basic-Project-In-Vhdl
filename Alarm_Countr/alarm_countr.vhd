c84322f40f45a6b8de8793cc2959e1fe5d43defe
eliohana101 <73431955+eliohana101@users.noreply.github.com>
2021-09-22 09:48:03 +0300

    Update alarm_countr.vhd

--- Alarm_Countr/alarm_countr.vhd
+++ Alarm_Countr/alarm_countr.vhd
@@ -1,23 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/15/2019 07:56:35 PM
--- Design Name: 
--- Module Name: alarm_countr - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
